15241cacc89b0e45931e84e682a846c0b76a4711
JINGE-ui <1378065021@qq.com>
2021-03-09 23:18:21 +0800

    lab3 exercise2 finished

--- kern/env.c
+++ kern/env.c
@@ -116,7 +116,13 @@ env_init(void)
 {
 	// Set up envs array
 	// LAB 3: Your code here.
-
+	env_free_list = NULL;
+	for(int i=NENV-1;i>=0;i--){
+		envs[i].env_id=0;
+		envs[i].env_status = ENV_FREE;
+		envs[i].env_link = env_free_list;
+		env_free_list = &envs[i];
+	}
 	// Per-CPU part of the initialization
 	env_init_percpu();
 }
@@ -179,7 +185,18 @@ env_setup_vm(struct Env *e)
 	//    - The functions in kern/pmap.h are handy.
 
 	// LAB 3: Your code here.
+	e->env_pgdir = (pde_t*)page2kva(p);
+	p->pp_ref++;	
 
+	//The VA space of all envs is identical above UTOP
+	for(i=PDX(UTOP);i<0X3FF;i++){
+		e->env_pgdir[i] = kern_pgdir[i];
+	}
+
+	//The initial VA below UTOP is empty
+	for(i=0;i<PDX(UTOP);i++){
+		e->env_pgdir[i]=0;	
+	}
 	// UVPT maps the env's own page table read-only.
 	// Permissions: kernel R, user R
 	e->env_pgdir[PDX(UVPT)] = PADDR(e->env_pgdir) | PTE_P | PTE_U;
@@ -267,6 +284,16 @@ region_alloc(struct Env *e, void *va, size_t len)
 	//   'va' and 'len' values that are not page-aligned.
 	//   You should round va down, and round (va + len) up.
 	//   (Watch out for corner-cases!)
+	void *start=(void*)ROUNDDOWN((uint32_t)va,PGSIZE);
+	void* end = (void*)ROUNDUP((uint32_t)va+len,PGSIZE);
+	struct PageInfo* p = NULL;
+	int r;
+	for(void* i=start;i<end;i+=PGSIZE){
+		p = page_alloc(0);
+		assert(p);
+		r = page_insert(e->env_pgdir,p,i,PTE_W|PTE_U);
+		assert(r==0);
+	}
 }
 
 //
@@ -328,6 +355,31 @@ load_icode(struct Env *e, uint8_t *binary)
 	// at virtual address USTACKTOP - PGSIZE.
 
 	// LAB 3: Your code here.
+	struct Elf* ELFheader = (struct Elf*)binary;
+	if(ELFheader->e_magic != ELF_MAGIC){
+		panic("the binary is not a ELF header!\n");
+	}
+	if(ELFheader->e_entry==0){
+		panic("the elf file can't be excuted!\n");	
+	}
+	e->env_tf.tf_eip = ELFheader->e_entry;
+	struct Proghdr *ph,*eph;
+	ph = (struct Proghdr*)((uint8_t*)ELFheader+ELFheader->e_phoff);
+	eph = ph + ELFheader->e_phnum;
+	
+	lcr3(PADDR(e->env_pgdir));
+	
+	for(;ph<eph;ph++){
+		if(ph->p_type == ELF_PROG_LOAD){
+			if(ph->p_memsz - ph->p_filesz < 0){
+				panic("p_memsz<p_filesz!\n");
+			}
+			region_alloc(e,	(void*)ph->p_va,ph->p_memsz);
+			memset((void*)ph->p_va,0,ph->p_memsz);
+			memcpy((void*)ph->p_va,binary+ph->p_offset,ph->p_filesz);
+		}
+	}
+	region_alloc(e,(void*)(USTACKTOP-PGSIZE),PGSIZE);
 }
 
 //
@@ -341,6 +393,13 @@ void
 env_create(uint8_t *binary, enum EnvType type)
 {
 	// LAB 3: Your code here.
+	struct Env* e;
+	int rc;
+	if((rc=env_alloc(&e,0)) !=0){
+		panic("env_craet falied!\n");
+	}
+	load_icode(e,binary);
+	e->env_type = type;
 }
 
 //
@@ -457,7 +516,15 @@ env_run(struct Env *e)
 	//	e->env_tf to sensible values.
 
 	// LAB 3: Your code here.
-
+	if(curenv != NULL && curenv->env_status == ENV_RUNNING){
+		curenv->env_status = ENV_RUNNABLE;	
+	}
+	curenv = e;
+	curenv->env_status = ENV_RUNNING;
+	curenv->env_runs++;
+	lcr3(PADDR(curenv->env_pgdir));
+	
+	env_pop_tf(&curenv->env_tf);
 	panic("env_run not yet implemented");
 }
 
