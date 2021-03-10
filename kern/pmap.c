fa06af83a65736be106f2b3a5553f965e2e6930d
JINGE-ui <1378065021@qq.com>
2021-03-10 16:14:02 +0800

    lab4 exercise4 finished

--- kern/pmap.c
+++ kern/pmap.c
@@ -271,12 +271,14 @@ mem_init_mp(void)
 	//     Permissions: kernel RW, user NONE
 	//
 	// LAB 4: Your code here:
-	uint32_t kstacktop_i;
-	for(int i=0;i<NCPU;i++){
+	int i;
+	uintptr_t kstacktop_i;
+	for(i=0;i<NCPU;i++){
 		kstacktop_i = KSTACKTOP - i*(KSTKSIZE+KSTKGAP);
 		boot_map_region(kern_pgdir,kstacktop_i-KSTKSIZE,
 			KSTKSIZE,PADDR(&percpu_kstacks[i]),PTE_W);
 	}
+	
 }
 
 // --------------------------------------------------------------
