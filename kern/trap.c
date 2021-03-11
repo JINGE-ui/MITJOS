3b3507276acd2d9a15b32628ca1f609eb85f1fa5
JINGE-ui <1378065021@qq.com>
2021-03-11 14:20:39 +0800

    lab4 exercise 14 finished

--- kern/trap.c
+++ kern/trap.c
@@ -250,7 +250,10 @@ trap_dispatch(struct Trapframe *tf)
 	// Handle clock interrupts. Don't forget to acknowledge the
 	// interrupt using lapic_eoi() before calling the scheduler!
 	// LAB 4: Your code here.
-
+	if(tf->tf_trapno == IRQ_OFFSET+IRQ_TIMER){
+		lapic_eoi();
+		sched_yield();
+	}
 	// Unexpected trap: The user process or the kernel has a bug.
 	// T_PGFLT: page fault
 	int32_t ret_code;
